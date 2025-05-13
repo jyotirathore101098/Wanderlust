c0fdcb334ee9bd8ba6f886a7e6eff5b7ce1994c5
Jyoti Rathore <jyotirathore101098@gmail.com>
2025-05-13 07:51:44 +0530

    update and add new files

--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-
+// const review = require('./review');
 const Schema=mongoose.Schema;
+const Review=require("./review.js");
 
 const listingSchema=new Schema({
     title:{
@@ -8,16 +9,6 @@ const listingSchema=new Schema({
         required:true,
     },
     description:String,
-    // image: {
-    //     filename: {
-    //         type: String,
-    //         required: true
-    //     },
-    //     url: {
-    //         type: String,
-    //         required: true
-    //     }
-    // },
     image:{
         type:String,
         default:"https://unsplash.com/photos/person-standing-on-paddle-board-on-body-of-water-DnbKhtkTboo",
@@ -25,7 +16,18 @@ const listingSchema=new Schema({
     },
     price:Number,
     location:String,
-    country:String
+    country:String,
+    reviews:[{
+        type:Schema.Types.ObjectId,
+        ref:"Review",
+    },
+    ],
+});
+
+listingSchema.post("findOneAndDelete",async(listing)=>{
+    if(listing){
+        await Review.deleteMany({_id:{$in: listing.reviews}});
+    }
 });
 const Listing=mongoose.model("Listing",listingSchema);
 module.exports=Listing;
