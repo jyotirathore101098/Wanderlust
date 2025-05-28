539929b22dc8ecb5e935af2fa38ece3cca1da295
Jyoti Rathore <jyotirathore101098@gmail.com>
2025-05-28 08:04:11 +0530

    login added

--- routes/user.js
+++ routes/user.js
@@ -14,8 +14,14 @@ router.post("/signup",wrapAsync(async(req,res)=>{
     const newUser=new User({email,username});
     const registeredUser=await User.register(newUser,password);
     console.log(registeredUser);
-    req.flash("success","Welcome to Wanderlust!");
-    res.redirect("/listings");
+    req.login(registeredUser,(err)=>{
+        if(err){
+            return next(err);
+        }
+        req.flash("success","Welcome to Wanderlust!");
+        res.redirect("/listings");
+    });
+    
     }catch(e){
         req.flash("error",e.message);
         res.redirect("/signup");
@@ -30,6 +36,16 @@ router.get("/login",(req,res)=>{
 router.post("/login",passport.authenticate("local", { failureRedirect: '/login',failureFlash:true }),async(req,res)=>{
     req.flash("success","Welcome to Wanderlust!");
     res.redirect("/listings");
+});
+
+router.get("/logout",(req,res,next)=>{
+    req.logOut((err)=>{
+        if(err){
+            return next(err);
+        }
+        req.flash("success","you are logged out");
+        res.redirect("/listings");
+    });
 });
 
 module.exports=router;
