2831dcb947ce866967a52c14755b8127caf67a85
Jyoti Rathore <jyotirathore101098@gmail.com>
2025-05-31 10:30:52 +0530

    review authorization added

--- routes/listing.js
+++ routes/listing.js
@@ -17,9 +17,18 @@ router.get("/",wrapAsync(async(req,res)=>{
  });
  
  // show route
- router.get("/:id",wrapAsync(async(req,res)=>{
-     let {id}=req.params;
-     const listing=await Listing.findById(id).populate("reviews").populate("owner");
+ router.get(
+    "/:id",
+    wrapAsync(async(req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id)
+     .populate({
+        path:"reviews",
+        populate:{
+            path:"author",
+        },
+      })
+    .populate("owner");
      if(!listing){
         req.flash("error","Listing you requested for does not exist!");
         res.redirect("/listings");
--- routes/review.js
+++ routes/review.js
@@ -4,13 +4,14 @@ const wrapAsync=require("../utils/wrapAsync.js");
 const ExpressError=require("../utils/ExpressError.js");
 const Review=require("../models/review.js");
 const Listing=require("../models/listing.js");
-const {validateReview}=require("../middleware.js");
+const {validateReview, isLoggedIn, isReviewAuthor}=require("../middleware.js");
 
 
 //Post Review Route
-router.post("/",validateReview,wrapAsync( async(req,res)=>{
+router.post("/",isLoggedIn,validateReview,wrapAsync( async(req,res)=>{
     let listing=await Listing.findById(req.params.id);
     let newReview=new Review(req.body.review);
+    newReview.author=req.user._id;
 
     listing.reviews.push(newReview);
 
@@ -27,7 +28,7 @@ router.post("/",validateReview,wrapAsync( async(req,res)=>{
 }));
 // Delete Review Route
 
-router.delete("/:reviewId",wrapAsync(async(req,res)=>{
+router.delete("/:reviewId",isLoggedIn,isReviewAuthor,wrapAsync(async(req,res)=>{
     let{id,reviewId}=req.params;
 
     await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
